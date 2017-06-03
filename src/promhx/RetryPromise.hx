7d9b9ab3589b30e35a771343bf4e3efc1df13182
Dion Amago <dion@transition9.com>
2017-06-03 10:00:52 -0700

    promhx helper tools

---
+++ src/promhx/RetryPromise.hx
@@ -0,0 +1,92 @@
+package promhx;
+
+import haxe.Json;
+
+import promhx.Deferred;
+import promhx.Promise;
+
+enum RetryType {
+	regular;
+	decaying;
+}
+
+class RetryPromise
+{
+	public static function retry<T>(f :Void->Promise<T>, type :RetryType, maxRetryAttempts :Int = 5, ?intervalMilliseconds :Int = 0, ?logPrefix :String = '', ?supressLogs :Bool= false) :Promise<T>
+	{
+		return switch(type) {
+			case regular: retryRegular(f, maxRetryAttempts, intervalMilliseconds, logPrefix, supressLogs);
+			case decaying: retryDecayingInterval(f, maxRetryAttempts, intervalMilliseconds, logPrefix, supressLogs);
+		}
+	}
+
+	public static function retryRegular<T>(f :Void->Promise<T>, maxRetryAttempts :Int = 5, ?intervalMilliseconds :Int = 0, ?logPrefix :String = '', ?supressLogs :Bool= false) :Promise<T>
+	{
+		var deferred = new promhx.deferred.DeferredPromise();
+		var attempts = 0;
+		var retryLocal = null;
+		retryLocal = function() {
+			attempts++;
+			var p = f();
+			if (p == null) {
+				throw 'RetryPromise.pollRegular f() returned a null promise';
+			}
+			p.then(function(val) {
+				if (attempts > 1 && !supressLogs) {
+					Log.debug('$logPrefix Success after $attempts');
+				}
+				deferred.resolve(val);
+			});
+			p.catchError(function(err) {
+				if (attempts < maxRetryAttempts) {
+					if (!supressLogs) {
+						Log.debug('$logPrefix Failed attempt $attempts err=${Json.stringify(err)}');
+					}
+					js.Node.setTimeout(retryLocal, intervalMilliseconds);
+				} else {
+					if (!supressLogs) {
+						Log.error('$logPrefix Failed all $maxRetryAttempts err=${Json.stringify(err)}');
+					}
+					deferred.boundPromise.reject(err);
+				}
+			});
+		}
+		retryLocal();
+		return deferred.boundPromise;
+	}
+
+	public static function retryDecayingInterval<T>(f :Void->Promise<T>, maxRetryAttempts :Int = 5, ?doublingRetryIntervalMilliseconds :Int = 0, logPrefix :String, ?supressLogs :Bool= false) :Promise<T>
+	{
+		var deferred = new promhx.deferred.DeferredPromise();
+
+		var attempts = 0;
+		var currentDelay = doublingRetryIntervalMilliseconds;
+		var retryLocal = null;
+		retryLocal = function() {
+			attempts++;
+			var p = f();
+			p.then(function(val) {
+				if (attempts > 1 && !supressLogs) {
+					Log.debug('$logPrefix Success after $attempts');
+				}
+				deferred.resolve(val);
+			});
+			p.catchError(function(err) {
+				if (attempts < maxRetryAttempts) {
+					if (!supressLogs) {
+						Log.debug('$logPrefix Failed attempt $attempts err=${Json.stringify(err)}');
+					}
+					js.Node.setTimeout(retryLocal, currentDelay);
+					currentDelay *= 2;
+				} else {
+					if (!supressLogs) {
+						Log.error('$logPrefix Failed all $maxRetryAttempts err=${Json.stringify(err)}');
+					}
+					deferred.boundPromise.reject(err);
+				}
+			});
+		}
+		retryLocal();
+		return deferred.boundPromise;
+	}
+}
