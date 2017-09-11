cad169b7d3ad37aa810e2cb02b5fa942bc4333fe
Dion Amago <dion@transition9.com>
2017-09-11 13:56:22 -0700

    More ArrayUtil methods

--- src/util/ArrayTools.hx
+++ src/util/ArrayTools.hx
@@ -4,6 +4,46 @@ import haxe.DynamicAccess;
 
 class ArrayTools
 {
+	public static function containsDuplicates(a :Array<Dynamic>) :Bool
+	{
+		if (a == null || a.length <= 1) {
+			return false;
+		} else {
+			var hasDuplicate = false;
+			for (i in 0...(a.length - 1)) {
+				for (j in (i + 1)...(a.length)) {
+					if (a[i] == a[j]) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+	}
+
+	public static function equalsDeep(a1 :Array<Dynamic>, a2 :Array<Dynamic>) :Bool
+	{
+		return if (a1 == null && a2 == null) {
+			true;
+		} else if ((a1 == null && a2 != null) || (a1 != null && a2 == null)) {
+			false;
+		} else if (a1.length != a2.length) {
+			false;
+		} else {
+			var i = 0;
+			//Assume equality
+			var equal = true;
+			while(i < a1.length) {
+				if (a1[i] != a2[i]) {
+					equal = false;
+					break;
+				}
+				i++;
+			}
+			equal;
+		}
+	}
+
 	public static function array<A>(it :Iterator<A>) :Array<A>
 	{
 		var a = new Array<A>();
