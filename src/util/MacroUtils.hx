16f5392f606ed2be00f0ec229e193099ce9b7420
Dion Amago <dion@transition9.com>
2018-07-29 21:22:13 -0700

    MacroUtils.getNpmVersion

--- src/util/MacroUtils.hx
+++ src/util/MacroUtils.hx
@@ -18,4 +18,18 @@ class MacroUtils
 	{
 		return macro $v{haxe.macro.Context.definedValue(key)};
 	}
+
+	macro public static function getNpmVersion(?path:String) :Expr
+	{
+		try {
+			var p = haxe.macro.Context.resolvePath(path == null ? './package.json' : path);
+			var version :String = haxe.Json.parse(sys.io.File.getContent(p)).version;
+			// an "ExprDef" is just a piece of a syntax tree. Something the compiler
+			// creates itself while parsing an a .hx file
+			return {expr: EConst(CString(version)) , pos : Context.currentPos()};
+		}
+		catch(e:Dynamic) {
+			return haxe.macro.Context.error('Failed to load file $path: $e', Context.currentPos());
+		}
+	}
 }
